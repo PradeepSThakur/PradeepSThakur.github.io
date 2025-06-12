798f26fff68a88688115ade4797668272bf5a735
madscientist-netizen <72627866+madscientist-netizen@users.noreply.github.com>
2025-06-13 04:41:56 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,8 +78,9 @@ for (let i = 0; i < selectItems.length; i++) {
 /// filter variables
 function getVisibleFilterItems() {
   const activePage = document.querySelector("[data-page].active");
-  return activePage.querySelectorAll("[data-filter-item]");
-}
+if (!activePage) return;  // just in case no page is active
+
+const filterItems = activePage.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
   const filterItems = getVisibleFilterItems();
